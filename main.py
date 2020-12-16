637ead9314f8b314a05e3dba79b95bc52c36e349
t.takahashi <takahashi.developer@gmail.com>
2020-12-17 00:39:57 +0900

    implement the process of converting text to voice

--- main.py
+++ main.py
@@ -2,13 +2,26 @@
 
 import math
 import urllib.parse
+from contextlib import closing
 from time import sleep
+
+import boto3
 
 from youtube_data_api_client import YoutubeDataApiClient
 
 YOUTUBE_DATA_CLIENT_SECRETS_FILE = "client_secrets.json"
 YOUTUBE_DATA_API_CLIENT_SCOPES = [
     'https://www.googleapis.com/auth/youtube.readonly']
+
+polly = boto3.client('polly')
+
+
+def convert_to_voice(path, text):
+    result = polly.synthesize_speech(Text=text, OutputFormat='mp3',
+                                     VoiceId='Joanna', Engine='neural')
+    with closing(result["AudioStream"]) as stream:
+        with open(path, 'wb') as file:
+            file.write(stream.read())
 
 
 def main():
@@ -23,8 +36,13 @@ def main():
     while True:
         live_chat_messages = youtube.get_live_chat_messages(
             live_chat_id, next_page_token)
+
+        for message in live_chat_messages['messages']:
+            convert_to_voice('voice.mp3', message)
+            # Raspberry PI で読ませる
+            print(message)
+
         next_page_token = live_chat_messages['next_page_token']
-        print(live_chat_messages['messages'])
         sleep(10)
 
 
